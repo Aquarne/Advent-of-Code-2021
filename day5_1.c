6a963967399cdfb40d19d5ee7025199bd29330bc
CynusW <davidbryansunjaya@gmail.com>
2021-12-05 16:10:29 +0700

    Day 5 done.

---
+++ day5_1.c
@@ -0,0 +1,240 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct point
+{
+    int x;
+    int y;
+};
+
+struct point_array
+{
+    int count;
+    int capacity;
+    struct point *points;
+};
+
+void point_array_init(struct point_array *array);
+void point_array_destroy(struct point_array *array);
+void point_array_add(struct point_array *array, struct point point);
+
+struct int_grid
+{
+    int width;
+    int height;
+    int *values;
+};
+
+void int_grid_init(struct int_grid *grid);
+void int_grid_destroy(struct int_grid *grid);
+void int_grid_set(struct int_grid *grid, int x, int y, int value);
+int int_grid_get(struct int_grid *grid, int x, int y);
+
+static void filter_points(struct point_array *array);
+static void draw_line(struct int_grid *grid, struct point p1, struct point p2);
+
+int main()
+{
+    int count = 0;
+    struct point_array points;
+    struct int_grid grid;
+
+    point_array_init(&points);
+    int_grid_init(&grid);
+
+    {
+        struct point p1, p2;
+
+        while (!feof(stdin))
+        {
+            scanf(
+                " %d %*c %d %*c%*c %d %*c %d ",
+                &p1.x, &p1.y, &p2.x, &p2.y
+            );
+            
+            point_array_add(&points, p1);
+            point_array_add(&points, p2);
+        }
+    }
+
+    filter_points(&points);
+
+    for (int i = 0; i < points.count; i += 2)
+    {
+        struct point p1 = points.points[i];
+        struct point p2 = points.points[i + 1];
+
+        if (p1.x > p2.x || p1.y > p2.y)
+        {
+            draw_line(&grid, p2, p1);
+        }
+        else
+        {
+            draw_line(&grid, p1, p2);
+        }
+    }
+
+    int grid_count = grid.width * grid.height;
+    for (int i = 0; i < grid_count; ++i)
+    {
+        count += grid.values[i] > 1;
+    }
+
+    printf("%d\n", count);
+
+    point_array_destroy(&points);
+    int_grid_destroy(&grid);
+
+    return 0;
+}
+
+static void filter_points(struct point_array *points)
+{
+    struct point_array new;
+    point_array_init(&new);
+
+    for (int i = 0; i < points->count; i += 2)
+    {
+        struct point p1 = points->points[i];
+        struct point p2 = points->points[i + 1];
+
+        if (p1.x == p2.x || p1.y == p2.y)
+        {
+            point_array_add(&new, p1);
+            point_array_add(&new, p2);
+        }
+    }
+
+    point_array_destroy(points);
+    *points = new;
+}
+
+static void draw_line(struct int_grid *grid, struct point p1, struct point p2)
+{
+    if (p1.x == p2.x)
+    {
+        int x = p1.x;
+        for (int y = p1.y; y <= p2.y; ++y)
+        {
+            int_grid_set(
+                grid, x, y,
+                int_grid_get(grid, x, y) + 1
+            );
+        }
+    }
+    else
+    {
+        int y = p1.y;
+        for (int x = p1.x; x <= p2.x; ++x)
+        {
+            int_grid_set(
+                grid, x, y,
+                int_grid_get(grid, x, y) + 1
+            );
+        }
+    }
+}
+
+void point_array_init(struct point_array *array)
+{
+    array->count = 0;
+    array->capacity = 0;
+    array->points = NULL;
+}
+
+void point_array_destroy(struct point_array *array)
+{
+    free(array->points);
+    point_array_init(array);
+}
+
+void point_array_add(struct point_array *array, struct point point)
+{
+    if (array->count + 1 > array->capacity)
+    {
+        array->capacity =
+            array->capacity < 25 ?
+                25 :
+                array->capacity * 2;
+        array->points = realloc(
+            array->points,
+            array->capacity * sizeof(struct point)
+        );
+    }
+
+    array->points[array->count] = point;
+    ++array->count;
+}
+
+void int_grid_init(struct int_grid *grid)
+{
+    grid->width = 0;
+    grid->height = 0;
+    grid->values = NULL;
+}
+
+void int_grid_destroy(struct int_grid *grid)
+{
+    free(grid->values);
+    int_grid_init(grid);
+}
+
+void int_grid_set(struct int_grid *grid, int x, int y, int value)
+{
+    while (x + 1 > grid->width)
+    {
+        int old_width = grid->width;
+        grid->width =
+            grid->width < 500 ?
+                500 :
+                grid->width * 2;
+
+        int *new_grid = malloc(grid->width * grid->height * sizeof(int));
+        for (int i = 0; i < grid->height; ++i)
+        {
+            int old_offset = i * old_width;
+            int offset = i * grid->width;
+
+            for (int j = 0; j < old_width; ++j)
+            {
+                new_grid[offset + j] = grid->values[old_offset + j];
+            }
+
+            for (int j = old_width; j < grid->width; ++j)
+            {
+                new_grid[offset + j] = 0;
+            }
+        }
+
+        free(grid->values);
+        grid->values = new_grid;
+    }
+
+    while (y + 1 > grid->height)
+    {
+        int old_endpoint = grid->width * grid->height;
+        grid->height =
+            grid->height < 500 ?
+                500 :
+                grid->height * 2;
+        grid->values = realloc(
+            grid->values,
+            grid->width * grid->height * sizeof(int)
+        );
+        
+        memset(grid->values + old_endpoint, 0, (grid->width * grid->height - old_endpoint) * sizeof(int));
+    }
+
+    grid->values[y * grid->width + x] = value;
+}
+
+int int_grid_get(struct int_grid *grid, int x, int y)
+{
+    if (x >= grid->width || y >= grid->height)
+    {
+        return 0;
+    }
+
+    return grid->values[y * grid->width + x];
+}
